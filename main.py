b1b23ff69d9573a906f601dd5d7363a084ff1ec4
Melissa Hale <melissakayh@gmail.com>
2023-07-06 13:55:41 -0500

    template

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 import os
+import json
 
 app = Flask(__name__)
+CORS(app)
+api = Api(app, '/thisapi')
 
+# Configure swagger
+SWAGGER_URL = '/thisapi/docs'
+API_URL = f'http://0.0.0.0:{os.getenv("PORT", default=5000)}/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'teamplate API'
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-@app.route('/flaskAPI/v1/properties')
-def index():
-    propertyList = [{
-        "address": "123 Main",
-        "bedrooms": 3,
-        "bathrooms": 1
-    }, {
-        "address": "456 Walnut",
-        "bedrooms": 2,
-        "bathrooms": 2
-    },
-    {
-        "address": "901 Walnut",
-        "bedrooms": 3,
-        "bathrooms": 2
-    }]
-    return jsonify(propertyList)
+@app.route('/swagger.json')
+def swagger():
+    with open('swagger.json', 'r') as f:
+        return jsonify(json.load(f))
 
+# define health check
+class HealthCheck(Resource):
+    def get(self):
+        return jsonify({"message": "All systems go."})
+# add endpoint to API
+api.add_resource(HealthCheck, '/health')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
